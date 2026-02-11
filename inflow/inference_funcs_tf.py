4075d4d4a8c84cd1dc2de5d69b72217979a1de2b
emisbrooke <91964118+emisbrooke@users.noreply.github.com>
2026-02-11 11:27:59 -0500

    Improved documentation

--- inflow/inference_funcs_tf.py
+++ inflow/inference_funcs_tf.py
@@ -1,50 +1,65 @@
 import torch 
 def calc_deri(t, theta, m, sigmas, lambda_ = 0.05):
     """
-    Get the derivative of the log likelihood with respect to theta and theta
-        t : latents, i.e. TFs, dim should be #TF x #cells 
-        theta: learned GRN dim is #TF x #non-TF genes
-        m: parameter to help match means, dim nG
-        sigmas: binary data, nTF x nC
+        Get the derivative of the log likelihood with respect to theta and m. Difference b/w tg is that diagonal entries must be 0
+        While the TF and TG models are found seperately, you can write the thetas together as one GRN with dimensions nTFx(nTF+nTG)
+        and we learn a sub-matrix with the constraint that TFs cannot regulate themselves 
+        Inputs-
+            t : latents, i.e. TFs, dim should be #TF x #cells 
+            theta: learned GRN dim is #TF_regulating x #TF_regulated genes here
+            m: parameter to help match means, dim nG
+            sigmas: binary data, nTF x nC. For the TF model, should be the same as t
+
+        Returns-
+            dLdtheta: gradient of likelihood w.r.t. theta
+            dLdm: radient of likelihood w.r.t. m
     """ 
     nTF = t.shape[0]
     idxs = torch.arange(nTF) # To keep the diagonal entries 0 (can't regulate themselves)
     
     exp = torch.mm(theta.transpose(0,1), t) + m.unsqueeze(1)    
     pi = torch.exp(-exp)/ ( 1+torch.exp(-exp))  # Newly calculate probabilites
-
     error = pi - sigmas # Error between predicted and actual
     
 
     ''' 
-    This is what the theta_{\ alpha, \ beta} looks like
+    This is what the theta_{alpha <- beta}.T looks like
+        -- we want tf_alpha = sum_beta(tf_beta)=theta.T*t + m_alpha 
+        -- its the sum of the effects of all the TFs (but alpha) on tf_alpha plus a term to learn mean of tf_alpha
 
-            tf1  tf2  tf3 .  .  .  tfN |
-        tf1  0                         |                           
-        tf2       0                    |                           
-        tf3            0               |
-        .                 .            |                           
-        .                    .         |                           
-        .                         .    |                           
-        tfN                          0 |                           
-        --------------------------------
+                        tf1  tf2  tf_beta ...tf_nTF|
+                    tf1  0                         |                           
+                    tf2       0                    |                           
+                    tf3_alpha      0               |
+        theta_.T =  .                 .            |                           
+                    .                    .         |                           
+                    .                         .    |                           
+                    tf_nTF                       0 |                           
+                    --------------------------------
 
     '''
     
     dLdtheta = torch.mm(t, error.transpose(0,1)) - lambda_* torch.sign(theta) # grad of theta with lasso reg
-    dLdtheta[idxs,idxs] = 0 # set diagonal entries to 0
+    dLdtheta[idxs,idxs] = 0 # ===== set diagonal entries to 0 =======
     
     dLdm = error.sum(axis=1)
 
     return dLdtheta, dLdm
 
 def calc_loglikelihood(sigmas,pi):
-    """calculates log likelihood likelihood """
+    """calculates log likelihood likelihood 
+    Input--
+        sigmas: data
+        pi: model probabilities 
+        the factor 1e-8 is here to ensure there are no nans in the calculation
+    Output--
+        L: log likelihood
+    """
     L = torch.sum(torch.multiply(sigmas, torch.log(pi+1e-8))) + torch.sum(torch.multiply((1-sigmas), torch.log(1-pi +1e-8)))  
     return L
 
 def adaptive_newparams(m, theta, mm, mtheta, vm,vtheta, dLdm, dLdtheta, beta1 =.9 , beta2  = .999, alpha = .1, i = 1, eps = 1e-8, optimizer  = 'adam'):
-    """Calculates the new parameters using the old parameters through ADAM/yogi algorithm"""
+    """Calculates the new parameters using the old parameters through ADAM/yogi algorithm (standard calculation)"""
     
     mm = beta1*mm + (1-beta1)*dLdm
     mtheta = beta1*mtheta + (1-beta1)*dLdtheta
--- inflow/inference_funcs_tg.py
+++ inflow/inference_funcs_tg.py
@@ -1,28 +1,38 @@
 import torch 
 def calc_deri(t,theta, m, sigmas, lambda_ = 0.05):
     """
-        Get the derivative of the log likelihood with respect to theta and theta
-        input: t : latents, i.e. TFs, dim should be #TF x #cells 
-        theta: learned GRN dim is #TF x #non-TF genes
+        Get the derivative of the log likelihood with respect to theta and m
+        While the TF and TG models are found seperately, you can write the thetas together as one GRN with dimensions nTFx(nTF+nTG)
+        and we learn a sub-matrix with the constraint that TG cannot regulate TFs
+        Inputs: 
+            t : latents, i.e. TFs, dim should be #TF x #cells 
+            m: parameter to help match means, dim nG
+            theta: learned GRN dim is #TF (nTF) x #non-TF genes (nG)
+            sigmas: binary data, #TGs x #cells. For the TG model this is the actual TG data
+
+        Returns-
+            dLdtheta: gradient of likelihood w.r.t. theta
+            dLdm: radient of likelihood w.r.t. m
     """ 
     
     exp = torch.mm(theta.transpose(0,1), t) + m.unsqueeze(1)    
     pi = torch.exp(-exp)/ ( 1+torch.exp(-exp))  
- 
     error = pi - sigmas
 
     ''' 
-    This is what the theta_{i, \ alpha} looks like, the 0,0 element is how much influence tf0 has on tg0
+    This is what the theta_{i<-alpha}.T looks like, the 0,0 element is how much influence tf0 has on tg0
+        -- we want tg_i = sum_alpha(tf_alpha) = theta.T*t + m_i 
+        -- its the sum of the effects of all the TFs on tg_i plus a term to learn mean of tg_i
 
-            tg0  tg1  tg2 .  .  . tgN |
-        tf0                           |                           
-        tf1                           |                           
-        tf2                           |
-        .                 .           |                           
-        .                    .        |                           
-        .                         .   |                           
-        tfN                           |                           
-        -------------------------------
+                    tf1  tf2 tf_alpha...tf_nTF|
+                tg0   .                       |                           
+                tg1        .                  |                           
+                tg_i           .              |
+        theta.T = .                 .         |                           
+                .                    .        |                           
+                .                         .   |                           
+                tg_nG                         |                           
+                -------------------------------
 
     '''
     
@@ -32,7 +42,14 @@ def calc_deri(t,theta, m, sigmas, lambda_ = 0.05):
     return dLdtheta, dLdm
 
 def calc_loglikelihood(sigmas,pi):
-    """calculates log likelihood likelihood """
+    """calculates log likelihood likelihood 
+    Input--
+        sigmas: data
+        pi: model probabilities 
+        the factor 1e-8 is here to ensure there are no nans in the calculation
+    Output--
+        L: log likelihood
+    """
     L = torch.sum(torch.multiply(sigmas, torch.log(pi+1e-8))) + torch.sum(torch.multiply((1-sigmas), torch.log(1-pi +1e-8)))
     return L 
 
